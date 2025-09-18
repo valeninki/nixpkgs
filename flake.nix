da19848875414ee48a4fa93097b701c161c70c1d
valeninki <valenmortus@protonmail.com>
2025-09-18 23:13:13 +0300

    Don't know what I'm doing.

--- flake.nix
+++ flake.nix
@@ -1,26 +1,18 @@
 {
-  description = "Valenpkgs's Flakes.";
+  description = "Valen's Private Flake for Custom Packages.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
-  outputs = {self, nixpkgs, ... }@inputs:
-  let
-    lib = nixpkgs.lib;
+  outputs = { self, nixpkgs, ... }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    valenpkgs = import ./pkgs;
-  in
-  {
-    nixosConfigurations.valenpkgs = lib.nixosSystem {
-      inherit system;
-      modules = [
-       ./pkgs.nix 
-      ];
-      specialArgs = {
-        inherit valenpkgs;
-      };
-    };    
+  in {
+    ${system} = rec {
+      topmem = pkgs.callPackage ./pkgs/applications/system/topmem/default.nix {};
+      zmem = pkgs.callPackage ./pkgs/applications/system/zmem/default.nix {};
+    };
   };
 }
+
