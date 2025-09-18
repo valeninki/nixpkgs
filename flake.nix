752904fad97ff5b6a38d74561d3ecd7b861a2364
valeninki <valenmortus@protonmail.com>
2025-09-18 23:26:06 +0300

    This is a living bomb.

--- flake.nix
+++ flake.nix
@@ -5,14 +5,24 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
-  outputs = { self, nixpkgs, ... }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    ${system} = rec {
-      topmem = pkgs.callPackage ./pkgs/applications/system/topmem/default.nix {};
-      zmem = pkgs.callPackage ./pkgs/applications/system/zmem/default.nix {};
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      valenpkgs = {
+        topmem = pkgs.callPackage ./pkgs/applications/system/topmem {};
+        zmem   = pkgs.callPackage ./pkgs/applications/system/zmem {};
+      };
+    in
+    {
+      nixosConfigurations.Parud = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          (import ./pkgs.nix { inherit config pkgs lib valenpkgs; })
+        ];
+      };
     };
-  };
 }
 
