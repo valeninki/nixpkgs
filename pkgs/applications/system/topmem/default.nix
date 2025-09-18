82b44d7b0ed24b87b050f5df08a4567f343a55dc
valeninki <valenmortus@protonmail.com>
2025-09-19 00:10:11 +0300

    Touched packages.

--- pkgs/applications/system/topmem/default.nix
+++ pkgs/applications/system/topmem/default.nix
@@ -1,28 +1,24 @@
-{ pkgs, ... }:
+{ pkgs }:
 
-let
-  topmem = pkgs.stdenv.mkDerivation {
-    pname = "topmem";
-    version = "6b32afb";
-    
-    src = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/CachyOS/CachyOS-Settings/refs/heads/master/usr/bin/topmem";
-      sha256 = "1xhg90vrzddn61nglbq1f20kzhsrqp67jv2zd535h9w9byrmkhci";
-    };
-    
-    buildInputs = [ 
-      (pkgs.lua5_3.withPackages (ps: with ps; [ luv ] ))
-    ];
-
-    dontUnpack = true;
+pkgs.stdenv.mkDerivation {
+  pname = "topmem";
+  version = "6b32afb";
   
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/topmem
-      chmod +x $out/bin/topmem
-    '';
+  src = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/CachyOS/CachyOS-Settings/refs/heads/master/usr/bin/topmem";
+    sha256 = "1xhg90vrzddn61nglbq1f20kzhsrqp67jv2zd535h9w9byrmkhci";
   };
-in
-{
-  environment.systemPackages = [ topmem ];
+  
+  buildInputs = [
+    (pkgs.lua5_3.withPackages (ps: with ps; [ luv ]))
+  ];
+
+  dontUnpack = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/topmem
+    chmod +x $out/bin/topmem
+  '';
 }
+
--- pkgs/applications/system/zmem/default.nix
+++ pkgs/applications/system/zmem/default.nix
@@ -1,25 +1,21 @@
-{ pkgs, ... }:
+{ pkgs }:
 
-let
-  zmemPkg = pkgs.rustPlatform.buildRustPackage rec {
+  pkgs.rustPlatform.buildRustPackage rec {
     pname = "zmem";
     version = "140d37e";
-  
-    src = pkgs.fetchFromGitHub {
-      owner = "xeome";
-      repo = pname;
-      rev = version;
-      sha256 = "sha256-qn/xfw/p2b6A2pyiTaxS7Nlu5Gk8D7qTDDQfJ+3BzDQ=";
-    };
-  
-    cargoHash = "sha256-pkBjVybga4yUmy8jpUfYi9i8eeRU2sIFEweDZLffP4o=";
-  
-    meta = {
-      description = "Advanced linux memory monitoring.";
-      homepage = "https://github.com/xeome/zmem";
-    };
+
+  src = pkgs.fetchFromGitHub {
+    owner = "xeome";
+    repo = pname;
+    rev = version;
+    sha256 = "sha256-qn/xfw/p2b6A2pyiTaxS7Nlu5Gk8D7qTDDQfJ+3BzDQ=";
   };
-in 
-  {
-  environment.systemPackages = [ zmemPkg ];
-  }
+
+  cargoHash = "sha256-pkBjVybga4yUmy8jpUfYi9i8eeRU2sIFEweDZLffP4o=";
+
+  meta = {
+    description = "Advanced linux memory monitoring.";
+    homepage = "https://github.com/xeome/zmem";
+  };
+}
+
