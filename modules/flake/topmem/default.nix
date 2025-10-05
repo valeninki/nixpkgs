fd23a1159dabe7f212293573dfea64806e143f2e
valeninki <valenmortus@protonmail.com>
2025-10-05 17:13:09 +0300

    Added 'config' to packages.

--- modules/flake/topmem/default.nix
+++ modules/flake/topmem/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 pkgs.stdenv.mkDerivation {
   pname = "topmem";
--- modules/flake/zmem/default.nix
+++ modules/flake/zmem/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
   pkgs.rustPlatform.buildRustPackage rec {
     pname = "zmem";
