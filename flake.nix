0c693ef358d589870a5d68bcb69fc23569a1af6a
valeninki <valenmortus@protonmail.com>
2025-10-05 16:57:29 +0300

    Trying something with using flake-parts template.

--- flake.nix
+++ flake.nix
@@ -2,35 +2,47 @@
   description = "Valen's Private Flake with custom packages";
 
   inputs = {
-    nixpkgs = {
+    nixpkgs {
       url = "github:NixOS/nixpkgs/nixos-25.05";
+    };
+    unixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
     };
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follow = "unixpkgs";
     };
   };
 
-  outputs = 
-  { self, nixpkgs, flake-parts, ... }: {
-    flakeModule = {
-      options= {
-        valenpkgs = flake-parts.lib.mkOption {
-	  type = nixpkgs.lib.types.bool;
-	  default = true;
-	};
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        ./modules/flake
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [ 
+        "x86_64-linux" 
+        "aarch64-linux" 
+      ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+	environment.systemPackages = with pkgs; [
+	  topmem
+	  zmem
+	];
       };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
 
-      config = { valenpkgs, lib, pkgs, ... }: {
-        imports = [
-	  ./modules/flake
-	];
-
-	environment.systemPackages = lib.mkIf valenpkgs [
-	  pkgs.zmem
-	  pkgs.topmem
-	];
       };
     };
-  };
 }
