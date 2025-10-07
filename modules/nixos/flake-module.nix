0f0603d1d79bca9da20cb50ea3a019680383e9d7
valeninki <valenmortus@protonmail.com>
2025-10-08 02:41:34 +0300

    Testing outputs 2.

--- modules/nixos/flake-module.nix
+++ modules/nixos/flake-module.nix
@@ -10,6 +10,6 @@
         self.packages.${pkgs.stdenv.hostPlatform.system}.valenpkgs
       ];
     };
-    valenpkgs = self.outputs.packages.${pkgs.stdenv.hostPlatform.system}.valenpkgs
+    valenpkgs = self.outputs.packages.${pkgs.stdenv.hostPlatform.system}.valenpkgs;
   };
 }
