b563c9f16b63e93499604d36a1de9e35eff7d740
valeninki <valenmortus@protonmail.com>
2025-10-08 13:08:04 +0300

    Changed packages.valenpkgs to zmem and topmem in flake-module.nix .

--- modules/nixos/flake-module.nix
+++ modules/nixos/flake-module.nix
@@ -1,14 +1,16 @@
 { self, lib, ...}: {
   
   perSystem = { config, self', inputs', pkgs, ... }: {
-    packages.topmem = pkgs.callPackage ./topmem {};
-    packages.zmem = pkgs.callPackage ./zmem {};
-    valenpkgs = self.outputs.packages.${pkgs.stdenv.hostPlatform.system}.valenpkgs;
+    packages = {
+      topmem = pkgs.callPackage ./topmem {};
+      zmem = pkgs.callPackage ./zmem {};
+    };
   };
   flake = {
     nixosModules.valenpkgs = { pkgs, ... }: {
       environment.systemPackages = [
-        self.packages.${pkgs.stdenv.hostPlatform.system}.valenpkgs
+        self.packages.${pkgs.stdenv.hostPlatform.system}.topmem
+	self.packages.${pkgs.stdenv.hostPlatform.system}.zmem
       ];
     };
   };
